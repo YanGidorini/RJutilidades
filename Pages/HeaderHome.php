4447fb2ce9393ea8f0a6eef3f4230d16f8fb7173
Renan Goulart <renan.goulart4@gmail.com>
2022-01-25 20:22:57 -0300

    Header e Card
    O header está perto de ser concluído, resta apenas alguns detalhes. Foi iniciado o protótipo do que será o card do produto. (No header as propriedades flex foram substituidas por grid)

--- Pages/HeaderHome.php
+++ Pages/HeaderHome.php
@@ -1,19 +1,22 @@
 <header>
-		<div class="container">
-			<div id="container-logo">
-				<picture>
-					<source type="" media="" srcset="">
-					<img src="Assets/IMG/Logo Vermelho - Fundo Branco 1.svg" alt="">
-				</picture>
-				<p>Papelaria, escritório, utilidades, miudezas...</p>
-			</div>
-			<div id="enfeite">
-				<nav>
-					<a href="home">Início</a>
-					<a href="produtos">Produtos</a>
-					<a href="contato">Contato</a>
-					<a href="favoritos">&#10084;</a>
-				</nav>
-			</div>
+	<div class="container">
+		<div id="container-logo">
+			<picture>
+				<source type="" media="" srcset="">
+				<img src="Assets/IMG/Logo Vermelho - Fundo Branco 1.svg" alt="">
+			</picture>
+			<p>Papelaria, escritório, utilidades, miudezas...</p>
 		</div>
-	</header>
+		<div id="fraseTitulo">
+			<h1>Tudo o que precisa, encontra aqui!</h1>
+		</div>
+		<div id="enfeite">
+			<nav>
+				<a href="home">Início</a>
+				<a href="produtos">Produtos</a>
+				<a href="contato">Contato</a>
+				<a href="favoritos">&#10084;</a>
+			</nav>
+		</div>
+	</div>
+</header>
--- Pages/Home.php
+++ Pages/Home.php
@@ -1,2 +1,15 @@
-<title>Home</title>
-<h1>Carregou a home</h1>
+<nav class="categorias">
+    <a href="#">Papelaria, Escritório e Escolar</a>
+    <a href="#">Artigos de Festa e Brinquedos</a>
+    <a href="#">Utensílios de Cozinha</a>
+    <a href="#">Miudezas</a>
+</nav>
+
+<section class="amostras">
+    <div class="cardProduto">
+        <div class="imgProduto"></div>
+        <h2>Lápis de Escrever Faber-Castell 60 cores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nisi quis dui facilisis faucibus et fermentum urna. Vivamus finibus.</p>
+        <span>R$ 14,80</span>
+    </div>
+</section>
