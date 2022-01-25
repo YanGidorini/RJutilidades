baf5d4d6ad0e511cfc5b61ca35de68d895d9223a
Renan Goulart <renan.goulart4@gmail.com>
2022-01-25 13:45:13 -0300

    Base do header
    Foi desenvolvido a base do que será o header final do site.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
@@ -10,7 +11,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,500;0,600;0,700;0,900;1,500;1,600;1,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="Assets/CSS/reset.css">
 	<link rel="stylesheet" href="Assets/CSS/style.css">
-	
+
 </head>
 
 <body>
@@ -21,42 +22,41 @@
 					<source type="" media="" srcset="">
 					<img src="Assets/IMG/Logo Vermelho - Fundo Branco 1.svg" alt="">
 				</picture>
-				<p>Papelaria, escritório, utilidades,miudezas...</p>
+				<p>Papelaria, escritório, utilidades, miudezas...</p>
 			</div>
-
-			<nav>
-				<a href="home">Início</a>
-				<p>Produtos</p> 
-				<a href="contato">Contato</a>
-				<a href="favoritos">&#10084;</a>
-			</nav>
+			<div id="enfeite">
+				<nav>
+					<a href="home">Início</a>
+					<a href="produtos">Produtos</a>
+					<a href="contato">Contato</a>
+					<a href="favoritos">&#10084;</a>
+				</nav>
+			</div>
 		</div>
-
 	</header>
 
-		<div id="enfeite">
-		
-		</div>
 
 
-	
-		
+
+
+
 
 
 	<?php
-		$url = (isset($_GET['url'])) ? $_GET['url']:'Home';
-		$url = array_filter(explode('/',$url));
-		var_dump($url);
+	$url = (isset($_GET['url'])) ? $_GET['url'] : 'Home';
+	$url = array_filter(explode('/', $url));
+	var_dump($url);
 
-		$file = 'Pages/'.ucfirst($url[0]).'.php';//define o filename
-		var_dump($file);
+	$file = 'Pages/' . ucfirst($url[0]) . '.php'; //define o filename
+	var_dump($file);
 
-		if(is_file($file)){
-			include $file;
-		}else{
-			include 'Pages/Home.php';
-		}
+	if (is_file($file)) {
+		include $file;
+	} else {
+		include 'Pages/Home.php';
+	}
 	?>
 
 </body>
+
 </html>
