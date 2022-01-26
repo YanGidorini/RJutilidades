17e40148b811fd05c03c88bd72d15faa35c10ef7
Renan Goulart <renan.goulart4@gmail.com>
2022-01-26 11:58:07 -0300

    Seção do Endereço
    Foi desenvolvido o protótipo do que será os primeiros produtos que vão aparecer no site, e o começo do que será a seção onde informará o endereço.

--- Pages/Home.php
+++ Pages/Home.php
@@ -12,4 +12,49 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nisi quis dui facilisis faucibus et fermentum urna. Vivamus finibus.</p>
         <span>R$ 14,80</span>
     </div>
+    <div class="cardProduto">
+        <div class="imgProduto"></div>
+        <h2>Lápis de Escrever Faber-Castell 60 cores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nisi quis dui facilisis faucibus et fermentum urna. Vivamus finibus.</p>
+        <span>R$ 14,80</span>
+    </div>
+    <div class="cardProduto">
+        <div class="imgProduto"></div>
+        <h2>Lápis de Escrever Faber-Castell 60 cores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nisi quis dui facilisis faucibus et fermentum urna. Vivamus finibus.</p>
+        <span>R$ 14,80</span>
+    </div>
+    <div class="cardProduto">
+        <div class="imgProduto"></div>
+        <h2>Lápis de Escrever Faber-Castell 60 cores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nisi quis dui facilisis faucibus et fermentum urna. Vivamus finibus.</p>
+        <span>R$ 14,80</span>
+    </div>
+    <div class="cardProduto">
+        <div class="imgProduto"></div>
+        <h2>Lápis de Escrever Faber-Castell 60 cores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nisi quis dui facilisis faucibus et fermentum urna. Vivamus finibus.</p>
+        <span>R$ 14,80</span>
+    </div>
+</section>
+
+<section class="secaoMapa">
+    <div class="mapa">
+        <h2>Visite nossa loja!</h2>
+        <div>
+            <p>Rua Jair de Godóy, nº56 - Vila Açoreana - Poá/SP</p>
+        </div>
+        <img src="Assets/IMG/mapa.png" alt="Endereço R&J Utilidades">
+    </div>
+
+    <div class="apps">
+        <div class="imgApp">
+            <img src="Assets/IMG/maps.svg" alt="Google Maps">
+        </div>
+        <p>Ver no Maps</p>
+        <div class="imgApp">
+            <img src="Assets/IMG/waze.svg" alt="Waze">
+        </div>
+        <p>Ver no Waze</p>
+    </div>
 </section>
